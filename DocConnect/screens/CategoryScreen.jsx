e7f6f8d07e0dafe32edddeb677905a4bc7cd8687
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2023-09-16 21:24:58 +0530

    Add files via upload

---
+++ DocConnect/screens/CategoryScreen.jsx
@@ -0,0 +1,136 @@
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { defaultStyle, colors } from "../constants/styles";
+import { allCategories } from "../constants/data";
+import { Avatar } from "react-native-paper";
+import SquareMenuButton from "../components/SquareMenuButton";
+
+const CategoryScreen = ({ navigation }) => {
+  const [visible, setVisible] = useState(false);
+  const closeMenu = () => {
+    setVisible(false);
+  };
+  const navigateTo = (item) => {
+    navigation.navigate(item.split(" ")[0]);
+  };
+  return (
+    <TouchableWithoutFeedback onPress={closeMenu}>
+      <View style={{ ...defaultStyle, padding: 0 }}>
+        <View
+          style={{
+            flex: 1,
+            position: "relative",
+          }}
+        >
+          <View style={styles.heading}>
+            <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+              <Avatar.Icon
+                icon={"arrow-left"}
+                style={{
+                  backgroundColor: colors.backgroundColor,
+                  resizeMode: "contain",
+                }}
+                color={colors.textColor}
+              />
+            </TouchableOpacity>
+            <SquareMenuButton onPress={() => setVisible(!visible)} />
+            {visible && (
+              <View style={styles.menu}>
+                <Text style={styles.boxTitle}>Help</Text>
+                <Text style={styles.boxTitle}>Contact Us</Text>
+              </View>
+            )}
+          </View>
+          <View style={{ height: "15%" }} />
+          <View style={{ marginBottom: 10 }}>
+            <Text
+              style={{
+                paddingHorizontal: 25,
+                fontSize: 30,
+                fontWeight: "bold",
+              }}
+            >
+              Categories
+            </Text>
+          </View>
+          <View
+            style={{
+              borderWidth: 1,
+              borderTopLeftRadius: 100,
+              borderColor: colors.textColor,
+              flex: 2,
+            }}
+          >
+            <View style={{ height: "10%" }} />
+            <View style={{ flex: 1, alignItems: "center" }}>
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={{ width: "85%" }}
+              >
+                {allCategories.map((item, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    style={{
+                      width: "100%",
+                      backgroundColor: colors.backgroundColor,
+                      borderRadius: 12,
+                      marginBottom: 15,
+                      flexDirection: "row",
+                      height: 60,
+                      elevation: 5,
+                      alignItems: "center",
+                      justifyContent: "center",
+                      borderWidth: 1,
+                    }}
+                    onPress={() => navigateTo(item)}
+                  >
+                    <Text style={{ textAlign: "center", fontSize: 20 }}>
+                      {item}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default CategoryScreen;
+
+const styles = StyleSheet.create({
+  heading: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    alignItems: "center",
+  },
+  menu: {
+    position: "absolute",
+    top: 60,
+    right: 25,
+    zIndex: 1,
+    backgroundColor: "#fff",
+    borderRadius: 7,
+    height: 70,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 5,
+  },
+  boxTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: colors.textColor,
+    textAlign: "center",
+  },
+});
