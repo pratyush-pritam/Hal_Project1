26396836f152557d14389f1336acd73a5affe9cb
Pratyush Pritam <73923732+pratyush-pritam@users.noreply.github.com>
2023-09-17 11:58:54 +0530

    Staff Screen added

---
+++ DocConnect/screens/StaffScreen.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Searchbar } from "react-native-paper";
+import { colors, defaultStyle } from "../constants/styles";
+
+const SearchScreen = () => {
+  const [query, setQuery] = useState("");
+  return (
+    <View style={{ ...defaultStyle, padding: 7 }}>
+      <Searchbar
+        placeholder="Search for doctors"
+        value={query}
+        onChangeText={setQuery}
+        style={{
+          backgroundColor: "#ffffff",
+          shadowColor: colors.textColor,
+        }}
+      />
+      {/* Doctors  */}
+    </View>
+  );
+};
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({});
